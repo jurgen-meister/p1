b1aae79b37bc584e2a394e47c510acba10941de1
Jurgen Cuiza <jurgencuiza@gmail.com>
2014-07-14 16:01:19 -0400

    Composer load code deleted from Config/bootstrap.php

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -179,5 +179,4 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
-CakePlugin::load('Composer', array('bootstrap' => true));
 CakePlugin::load('TwitterBootstrap');
